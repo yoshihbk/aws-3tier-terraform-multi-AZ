169fc7458f13d090b382c5544f691032785fc6df
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-15 10:55:26 +0900

    Add ALB listener

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -37,3 +37,14 @@ resource "aws_lb_target_group" "web_tg" {
     Name = "web-tg"
   }
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
