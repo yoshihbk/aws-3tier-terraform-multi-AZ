1dc9ced5d6fb87352329b3eb126795e02f058671
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-15 14:54:34 +0900

    Add complete variable definitions for region, VPC, subnets, AZs, and instance type

---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  default = ["10.0.0.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "default_tags" {
+  type = map(string)
+  default = {
+    Project = "3tier-architecture"
+    Owner   = "Yoshihiro"
+  }
+}
